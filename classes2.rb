78eb82d22a4c384c8a66434bc94e8ec429507eee
Ari Drookman <aga.drookman@gmail.com>
2019-12-29 06:03:36 -0500

    Complete

--- classes2.rb
+++ classes2.rb
@@ -28,8 +28,32 @@ class Animal
 		return @speed
 	end
 
+	def get_size
+		return @size
+	end
+
+	def get_num_of_legs
+		return @num_of_legs
+	end
+
+	def get_height
+		return @height
+	end
+
 	def set_speed(new_speed) # Setter
 		@speed = new_speed
+	end
+
+	def set_size(new_size)
+		@size = new_size
+	end
+
+	def set_num_of_legs(new_nol)
+		@num_of_legs = new_nol
+	end
+
+	def set_height(new_height)
+		@height = new_height
 	end
 end
 
@@ -39,6 +63,15 @@ fish = Animal.new(100, 10, 2, 200)
 # puts fish.speed
 
 # Correct: Using a getter
-puts fish.get_speed
-fish.set_speed(200)
-puts fish.get_speed
+puts cat.get_speed
+puts cat.get_size
+puts cat.get_num_of_legs
+puts cat.get_height
+cat.set_speed(30)
+cat.set_size(0.46)
+cat.set_num_of_legs(4)
+cat.set_height(0.35)
+puts cat.get_speed
+puts cat.get_size
+puts cat.get_num_of_legs
+puts cat.get_height
